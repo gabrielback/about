8de33488fbd9a405f59ac31332567632e919e46b
Gabriel Linux Mint <gabrielluiz-back@hotmail.com>
2022-06-20 01:14:26 -0300

    Update Projeto

--- js/index.js
+++ js/index.js
@@ -7,8 +7,14 @@ headerList.forEach((navButton) => navButton.addEventListener("click", () => {
 }
 ))
 
+$('.formulario').hide(0).delay(15000).css("right","5px").slideDown(1000)
 
-// AJAX
+$('.x').click(function(){
+    $('.formulario').slideUp(1000)
+})
+
+
+
 function openPage(tagName, pageHTML) {
     pageHTML = `./pages/${pageHTML}`
     var xml = new XMLHttpRequest()
@@ -26,19 +32,3 @@ function openPage(tagName, pageHTML) {
     .fadeIn(700)
 
 }
-
-
-        // // Jquery é universalmente referenciado com o dólar "$"
-        // $('div').hide(1000).show(5000).fadeOut(5000) // seletor
-        // console.log($('div').get(0))
-        // console.log($.isEmptyObject({nome: null}))
-        // console.log($.isEmptyObject({}))
-        // console.log(jQuery.inArray(3, [1, 2, 3]))
-        // const array1 = [55, 44, 33] // retorna posição do array, caso não esteja presente, retorna -1
-        // console.log($.inArray(44, array1))
-        // console.log($.inArray(45, array1)) // retorna -1. Não está presente no array.
-        // $.fn.bgColor = function (){
-        //     this.css('background-color', 'green')
-        //     return this
-        // }
-        // $('body').bgColor().fadeOut(1000)
